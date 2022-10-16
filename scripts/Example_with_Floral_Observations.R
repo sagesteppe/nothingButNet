cf764212f77d161ce7c033d1a2028a49934d4734
sagesteppe <reedbenkendorf27@gmail.com>
2022-10-16 14:08:13 -0600

    setting up files in the standard manner and adding roxygen formatting

---
+++ scripts/Example_with_Floral_Observations.R
@@ -0,0 +1,120 @@
+library(here)
+library(tidyverse)
+library(igraph)
+# set_here('~/Documents/floral_observations')  # move this to be the root of the project folder on your box
+setwd('~/Documents/floral_observations')
+
+p2d <- 'data'
+files <- list.files(p2d, pattern = 'csv')
+
+flr_ranks <- read.csv(
+  paste0(p2d, '/', files[grep('flower_ranks', files)] )) %>% 
+  select(site, doy, week, tot.obs.length, habitat, plant.species:abun.rank)
+
+observation_times <- read.csv(
+  paste0(p2d, '/', files[grep('queen_observations', files)]) ) %>% 
+  select(site, week, tot.obs.length) %>% 
+  distinct(site, week, .keep_all = T)
+
+bee_obs <- read.csv(
+  paste0(p2d, '/', files[grep('queen_observations', files)]) ) %>% 
+  filter(caste == 'q') %>% 
+  mutate(species = gsub('\\..*$', '', species)) %>% 
+  select(site, doy, week, tot.obs.length, species, plant.species,
+         resource.coll:pollen.collected, fl.switch.1:fl.switch.2.resource.coll) %>% 
+  mutate(across(resource.coll:fl.switch.2.resource.coll, ~ na_if(.x, ''))) %>% 
+  pivot_longer(c(plant.species, fl.switch.1, fl.switch.2), 
+               names_to = 'observation', values_to = 'plant.species') %>% 
+  drop_na(plant.species) %>% 
+  group_by(site, week, species, plant.species) %>% 
+  count(name = 'interactions') %>% 
+  left_join(., observation_times) %>% 
+  mutate(period = case_when(
+    week %in% 3:5 ~ 'Early',
+    week %in% 6:8 ~ 'Mid',
+    week %in% 9:11 ~ 'Late'
+  ))
+
+arranged_plants <- c('C.gunnisonii', 'E.grandiflorum',
+                     
+                     'A.columbianum', 'A.coerulea', "T.fendleri",
+                     'D.barbeyi', 'D.nuttallianum',
+                     
+                     "L.leucanthus", 'L.bakeri', "V.americana",
+                     
+                     "E.triflora","P.pulcherrima",
+                     
+                     "M.ciliata" ,"M.fusiformis",
+                     
+                     "A.lewisii",
+                     "C.lanceolata",
+                     "D.involucrata",
+                     "S.sp",
+                     "V.praemorsa", # ???/
+                     
+                     "H.capitatum", "H.fendleri",
+                     "A.urticifolia", "P.procera", "P.bracteosa",
+                     
+                     "C.parryi", "D.hoopesii", "H.quinquenervis", "L.bigelovii",
+                     "E.speciosus", "S.integerrimus", "T.officinale", "W.arizonica",
+                     
+                     "F.speciosa",
+                     "V.occidentalis",
+                     "D.pulchellum",
+                     'O.occidentalis'
+)
+
+arranged_bees <- c('bifarius', 'mixtus', 'rufocinctus', 'sylvicola',
+                   'flavifrons',
+                   'appositus', 'californicus', 'nevadensis', 
+                   'unknown')
+
+rm(p2d, files, observation_times)
+
+bee_obs_wk <- bee_obs %>% 
+  ungroup() %>% 
+  group_by(species, plant.species, period) %>% 
+  mutate(Interactions_total  = sum(interactions)) %>% 
+  distinct(species, plant.species, period, .keep_all = T) %>% 
+  select(species, plant.species, period, Interactions_total, tot.obs.length) %>% 
+  ungroup(plant.species) %>% 
+  mutate(n = sum(Interactions_total),
+         Prop = Interactions_total/n)
+
+resin <- bee_obs_wk %>% 
+  select(-tot.obs.length, -n, -Prop) %>%
+  mutate(plant.species = paste0(substr(plant.species, 1,1), '. ',
+                                gsub("^.*\\.", "", plant.species ))) %>% 
+  pivot_wider(names_from = plant.species, values_from = Interactions_total) %>% 
+  mutate(across(everything(), ~replace_na(.x, 0))) %>% 
+  arrange(period, species) %>% 
+  split(., f = .$period) %>% 
+  lapply(., column_to_rownames, 'species') %>% 
+  lapply(., select, -period) %>% 
+  lapply(., as.matrix)
+
+
+
+# networks
+
+tet <- lapply(resin, arrange_nets, col_arrange = arranged_plants, 
+              row_arrange = arranged_bees)
+rm(bee_obs_wk, resin, bee_obs)
+
+early <- tet[['Early']]
+mid <- tet[['Mid']]
+late <- tet[['Late']]
+
+graphDrawer(mid, lbl_fnt = 14,
+            plot_name = 'mid',
+            edge_clr = 'lightseagreen',
+            node_clrs  = c("#CEAB07", "deeppink2"),
+            legend_items = c("Bombus", "Plant"),
+            fname = 'floral-mid', 
+            ntwrks_page = 12,
+            col = 3
+)
+
+
+
+names(late)
--- scripts/functions.R
+++ scripts/functions.R
@@ -1,111 +1,17 @@
-library(here)
-library(tidyverse)
-library(igraph)
-# set_here('~/Documents/floral_observations')  # move this to be the root of the project folder on your box
-setwd('~/Documents/floral_observations')
-
-p2d <- 'data'
-files <- list.files(p2d, pattern = 'csv')
-
-flr_ranks <- read.csv(
-  paste0(p2d, '/', files[grep('flower_ranks', files)] )) %>% 
-  select(site, doy, week, tot.obs.length, habitat, plant.species:abun.rank)
-
-observation_times <- read.csv(
-  paste0(p2d, '/', files[grep('queen_observations', files)]) ) %>% 
-  select(site, week, tot.obs.length) %>% 
-  distinct(site, week, .keep_all = T)
-
-bee_obs <- read.csv(
-  paste0(p2d, '/', files[grep('queen_observations', files)]) ) %>% 
-  filter(caste == 'q') %>% 
-  mutate(species = gsub('\\..*$', '', species)) %>% 
-  select(site, doy, week, tot.obs.length, species, plant.species,
-         resource.coll:pollen.collected, fl.switch.1:fl.switch.2.resource.coll) %>% 
-  mutate(across(resource.coll:fl.switch.2.resource.coll, ~ na_if(.x, ''))) %>% 
-  pivot_longer(c(plant.species, fl.switch.1, fl.switch.2), 
-               names_to = 'observation', values_to = 'plant.species') %>% 
-  drop_na(plant.species) %>% 
-  group_by(site, week, species, plant.species) %>% 
-  count(name = 'interactions') %>% 
-  left_join(., observation_times) %>% 
-  mutate(period = case_when(
-    week %in% 3:5 ~ 'Early',
-    week %in% 6:8 ~ 'Mid',
-    week %in% 9:11 ~ 'Late'
-  ))
-
-arranged_plants <- c('C.gunnisonii', 'E.grandiflorum',
-  
-  'A.columbianum', 'A.coerulea', "T.fendleri",
-  'D.barbeyi', 'D.nuttallianum',
-  
-  "L.leucanthus", 'L.bakeri', "V.americana",
-  
-  "E.triflora","P.pulcherrima",
-  
-  "M.ciliata" ,"M.fusiformis",
-  
-  "A.lewisii",
-  "C.lanceolata",
-  "D.involucrata",
-  "S.sp",
-  "V.praemorsa", # ???/
-  
-  "H.capitatum", "H.fendleri",
-  "A.urticifolia", "P.procera", "P.bracteosa",
-  
-  "C.parryi", "D.hoopesii", "H.quinquenervis", "L.bigelovii",
-  "E.speciosus", "S.integerrimus", "T.officinale", "W.arizonica",
-  
-  "F.speciosa",
-  "V.occidentalis",
-  "D.pulchellum",
-  'O.occidentalis'
-  )
-
-arranged_bees <- c('bifarius', 'mixtus', 'rufocinctus', 'sylvicola',
-                   'flavifrons',
-                   'appositus', 'californicus', 'nevadensis', 
-                   'unknown')
-
-rm(p2d, files, observation_times)
-
-bee_obs_wk <- bee_obs %>% 
-  ungroup() %>% 
-  group_by(species, plant.species, period) %>% 
-  mutate(Interactions_total  = sum(interactions)) %>% 
-  distinct(species, plant.species, period, .keep_all = T) %>% 
-  select(species, plant.species, period, Interactions_total, tot.obs.length) %>% 
-  ungroup(plant.species) %>% 
-  mutate(n = sum(Interactions_total),
-    Prop = Interactions_total/n)
-
-resin <- bee_obs_wk %>% 
-  select(-tot.obs.length, -n, -Prop) %>%
-  mutate(plant.species = paste0(substr(plant.species, 1,1), '. ',
-                                gsub("^.*\\.", "", plant.species ))) %>% 
-  pivot_wider(names_from = plant.species, values_from = Interactions_total) %>% 
-  mutate(across(everything(), ~replace_na(.x, 0))) %>% 
-  arrange(period, species) %>% 
-  split(., f = .$period) %>% 
-  lapply(., column_to_rownames, 'species') %>% 
-  lapply(., select, -period) %>% 
-  lapply(., as.matrix)
-
+#' Rearrange elements of a network thematically
+#' 
+#' 'arrange_nets()' helps standardize the order of species in these networks.
+#' We use two different schema to arrange our taxa in one net! The insects we arrange
+#' based on functional parameters, in this case proboscis length. The plants we 
+#' arrange using a phylogenetic and alphabetical approach. These arguments must be 
+#' given as character vectors with names in the EXACT SAME format as the input data.
+#' 
+#' @param x = a network in split format
+#' @param col_arrange = a character vector with it's elements in the order which you want the columns arranged (all column names must be in the vector, and be in the format 'GENUS.EPITHET' (note no spaces))
+#' @param row_arrange = a character vector with it's elements in the order which you want the rows arranged (all row names must be in the vector)
+#' @export
 arrange_nets <- function(x, col_arrange, row_arrange){
   
-  #' This function serves to help standardize the order of species in these networks.
-  #' We use two different schema to arrange our taxa in one net! The insects we arrange
-  #' based on functional parameters, in this case proboscis length. The plants we 
-  #' arrange using a phylogenetic and alphabetical approach. These arguments must be 
-  #' given as character vectors with names in the EXACT SAME format as the input data.
-  #' 
-  #' INPUTS: 
-  #' x = a network in split format
-  #' col_arrange = a character vector with it's elements in the order which you want the columns arranged (all column names must be in the vector, and be in the format 'GENUS.EPITHET' (note no spaces))
-  #' row_arrange = a character vector with it's elements in the order which you want the rows arranged (all row names must be in the vector)
-  #' 
   if(missing(row_arrange)) { row_arrange <- rownames(x) }
   
   x <- x[rowSums(x) > 0, colSums(x) > 0]
@@ -122,6 +28,19 @@ arrange_nets <- function(x, col_arrange, row_arrange){
   return(x)
   
 }
+
+
+#' cleanup column and row names from blanker
+#' Create information rich circular networks
+#' 
+#' 'blanker()' inserts several 'empty' observations into the rows and/or columns of
+#' a matrix of interaction data so that nodes can be labelled at the top and bottom
+#' of an igraph circular graph.
+#' 
+#' @param x = a network in matrix format
+#' @returns = a network in matrix format with blank rows/columns inserted
+#' @seealso 'vertex_names', 'graphDrawer'
+#' @export
 blanker <- function(x){
   
   netL <- nrow(x) + ncol(x) 
@@ -171,6 +90,16 @@ blanker <- function(x){
   names(p) <- gsub('\\.\\.', '\\.', names(p))
   return(p)
 }
+
+
+#' Clean up element names from 'blanker'
+#' 
+#' After inserting the blank 'observations' into the graph to act as 
+#' vertices we will remove their row and column names to hide the 
+#' dummy names R gives them by default.
+#' @param x = a matrix output from 'blanker'
+#' @seealso 'blanker', 'graphDrawer'
+#' @export
 vertex_names <- function(x){
   
   #' After inserting the blank 'observations' into the graph to act as 
@@ -188,12 +117,20 @@ vertex_names <- function(x){
   
   vertice_names <- gsub("\\.(?=[A-Za-z])", ". ", vertice_names, perl = TRUE)
 }
+
+
+#' Give node labels rendering space
+#' 
+#' Takes a single input, a network, this will serve to specify the positions
+#' for the labels of objects in it. It does not modify the label values, just
+#' positions and orientation. NOTE: some extra terms are left in the function
+#' for modifications of the upper and lower positions
+#' 
+#' @param x = a matrix output from vertex_names
+#' @seealso 'vertex_names', 'graphDrawer'
+#' @export
 vert_lab_position <- function(x){
   
-  #' Takes a single input, a network, this will serve to specify the positions
-  #' for the labels of objects in it. It does not modify the label values, just
-  #' positions and orientation. NOTE: some extra terms are left in the function
-  #' for modifications of the upper and lower positions
   
   nodes <- nrow(x) + ncol(x)
   degrees <- 360/nodes
@@ -230,6 +167,20 @@ vert_lab_position <- function(x){
   
   return(labels)
 }
+
+#' Custom colouring for the net
+#' 
+#' This serves to set the colors of up to three groups in the plot
+#' the first two groups being those major groups which interact in the 
+#' network. the final group is the background color of the plot. This will
+#' default to a white if left blank.
+#' @param x = the matrix output from 'blanker'
+#' @param net = network from 'igraph::graph_from_incidence_matrix()'
+#' @param node_clrs = a vector of length two of hex or named colors for the major groups
+#' @param bg_clr = background color for the plot
+#' @param VNames = a vector of names from 'vertex_names' function
+#' @seealso = 'vertex_names', 'blanker', 'graphDrawer'
+#' @export
 set_colors <- function(x, net, node_clrs,  bg_clr, VNames){
   
   #' This serves to set the colors of up to three groups in the plot
@@ -251,12 +202,18 @@ set_colors <- function(x, net, node_clrs,  bg_clr, VNames){
   return(net)
   
 }
+
+#' Size graphs for assembling on an A4 page
+#' 
+#' this function hopefully returns square graphs of similar size which may be
+#' somewhat readily assembled onto an A4 page with what i hope are standard
+#' page margins (2.97cm top & bottom, 2.1 cm sides). It is easily modifiable for
+#' alternative dimensions. It assumes you want to fill an entire page with graphs.
+#' @param ntwrks_page = the number of networks to place on the page
+#' @col = the number of columns to spread the networks across
+#' @seealso = 'graphDrawer'
+#' @export
 graph_dims <- function(ntwrks_page, col){
-  
-  #' this function hopefully returns square graphs of similar size which may be
-  #' somewhat readily assembled onto an A4 page with what i hope are standard
-  #' page margins (2.97cm top & bottom, 2.1 cm sides). It is easily modifiable for
-  #' alternative dimensions. It assumes you want to fill an entire page with graphs.
   
   W = round(1984 / col, 0)
   H = round(2864 / (ntwrks_page/col), 0 )
@@ -267,32 +224,42 @@ graph_dims <- function(ntwrks_page, col){
   return(dims)
 }
 
+
+#' Render multiple graphs of similar thematic content
+#' 
+#' this function serves to draw multiples of similar graphs, it's most logical 
+#' applications are in displaying information across temporal slices, or 
+#' spatial replicates or treatments thereof. It relies on most of the functions in 
+#' package.
+#' 
+#' @param data = a list of matrcies, wherein one group of interacting organisms 
+#' (e.g.) insects are along one axis, like rows, and the other group of organisms
+#'  is along the other axis. This will be coerced to an igraph object. Named 
+#'  lists may serve as titles of the rendered plots. 
+#' @param edge_clr, a color for mapping the edges (i.e. interactions) between 
+#' the nodes (i.e. species)
+#' @param node_clrs, a set of two colors for your major groups
+#' @param bg_clr, a background color for the network, defaults to white
+#' @param label_fnt_size, label font size defaults to 9
+#' @param legend item, the name of the two groups on the axis, 
+#' e.g. c('Insects', 'Plants')
+#' @param directory = directory to save graphs to, defaults to 'NetworkGraphs'
+#' @param fname = filename for the output graph, defaults to name of input ('data')
+#' @param ntwrks_page = (numeric) how many networks per page? Defaults to 1
+#' @param col = (numeric) how many columns of networks? Defaults to 1
+#' @param H = (numeric) height of graph in pixels. If left blank is calculated
+#'  to maximize symmetrical space on an A4 page.
+#' @param graph sizes on an A4 page with standard margins based on 'ntwrks_page'
+#'  and 'col'
+#' @param W = (numeric) length of graph in pixels. If left blank is calculated 
+#' to maximixe graph sizes on an A4 page with standard margins based on 
+#' 'ntwrks_page' and 'col'
+#' @seealso 'blanker', 'vertex_names, vert_lab_position','set_colors',
+#' 'graph_dims'
+#' @export
 graphDrawer <- function(data, plot_name, edge_clr, node_clrs,  bg_clr, 
                         lbl_fnt, legend_items, directory, fname,
                         ntwrks_page, col, H, W){
-  
-  #' this function serves to draw multiples of similar graphs, it's most logical 
-  #' applications are in displaying information across temporal slices, or 
-  #' spatial replicates or treatments thereof.
-  #' 
-  #' INPUTS
-  #' data = a list of dataframes, wherein one group of interacting organisms (e.g.)
-  #' insects are along one axis, like rows, and the other group of organisms is 
-  #' along the other axis. This will be coerced to an igraph object. Named lists may 
-  #' serve as titles of the rendered plots. 
-  #' edge_clr, a color for mapping the edges (interactions) between the nodes (species)
-  #' node_clrs, a set of two colors for your major groups
-  #' bg_clr, a background color for the network, defaults to white
-  #' label_fnt_size, label font size defaults to 9
-  #' legend item, the name of the two groups on the axis, e.g. c('Insects', 'Plants')
-  #' directory = name of directory to save graphs to, defaults to 'NetworkGraphs'
-  #' fname = filename for the output graph, defaults to name of input
-  #' ntwrks_page = (numeric) how many networks per page? Defaults to one
-  #' col = (numeric) how many columns of networks? Defaults to one
-  #' H = (numeric) heigh of graph in pixels. If left blank is calculated to maximixe 
-  #' graph sizes on an A4 page with standard margins based on 'ntwrks_page' and 'col'
-  #' W = (numeric) length of graph in pixels. If left blank is calculated to maximixe 
-  #' graph sizes on an A4 page with standard margins based on 'ntwrks_page' and 'col'
   
   if(missing(plot_name)) { plot_name <- substitute(data) }
   if(missing(lbl_fnt)) { lbl_fnt <- 14 }
@@ -322,8 +289,8 @@ graphDrawer <- function(data, plot_name, edge_clr, node_clrs,  bg_clr,
   VNames <- vertex_names(blanked_data)
   VLPs <- vert_lab_position(blanked_data)
   
-  net = graph_from_incidence_matrix(blanked_data, weight=T)
-  deg = centr_degree(net, mode="all")
+  net = igraph::graph_from_incidence_matrix(blanked_data, weight=T)
+  deg = igraph::centr_degree(net, mode="all")
   
   net <- set_colors(x = blanked_data, net, node_clrs, VNames = VNames)
   
@@ -353,27 +320,3 @@ graphDrawer <- function(data, plot_name, edge_clr, node_clrs,  bg_clr,
  message(paste0("'", plot_name, "' has been rendered as a graph and saved to:\n ", filename))
 
 }
-
-# networks
-
-tet <- lapply(resin, arrange_nets, col_arrange = arranged_plants, 
-              row_arrange = arranged_bees)
-rm(bee_obs_wk, resin, bee_obs)
-
-early <- tet[['Early']]
-mid <- tet[['Mid']]
-late <- tet[['Late']]
-
-graphDrawer(mid, lbl_fnt = 14,
-            plot_name = 'mid',
-            edge_clr = 'lightseagreen',
-            node_clrs  = c("#CEAB07", "deeppink2"),
-            legend_items = c("Bombus", "Plant"),
-            fname = 'floral-mid', 
-            ntwrks_page = 12,
-            col = 3
-)
-
-
-
-names(late)
